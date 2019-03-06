0894055bfc4e2f266a1602e09683a6f7707debed
Brandon Chan <iambrandonchan@gmail.com>
2019-03-05 20:36:38 -0600

    added some commandline arguments

--- progressor.py
+++ progressor.py
@@ -1,6 +1,41 @@
 from tqdm import tqdm
 import time
 import os
+import argparse
+import sys
+
+def _parse_args():
+
+    parser = argparse.ArgumentParser(description='progressor.py')
+    parser.add_argument('-f', type=str, default=None, help='file to add tqdm, is required')
+    parser.add_argument('-p', action='store_true', help='permanently add tqdm to the specified file')
+    parser.add_argument('-c', action='store_true', help='make a copy of the tqdm file')
+    parser.add_argument('-r', action='store_true', help='run the modified file')
+
+    args = parser.parse_args()
+    return args
+
+def handleArguments(args):
+	# kill the program if there is no such target file
+	if args.f == None:
+		sys.exit('no such target file..., specify path to file, use -f')
+	# need to check if the file exists in the cwd, and if it's a python file
+	elif not os.path.isfile(args.f) or not '.py' in args.f:
+		sys.exit('invalid file..., must be a .py file')
+	# we have a valid file
+	else:
+		print('success!')
+		# we now need to handle the rest of the flags
+		if args.p:
+			print('p')
+		elif args.c:
+			print('c')
+		elif args.r:
+			print('r')
+		else:
+			print('r')
+			args.r == True
+	return args
 
 
 def readLinesFromFile():
@@ -60,13 +95,22 @@ def addTQDM(fileLines):
 	fileLines = addTQDMLines(fileLines)
 	return fileLines
 
-def writeLinesToFile(lines):
-	with open('test.py', 'w') as f:
+
+def writeLinesToFile(lines, args):
+	if args.c:
+		args.f = 'tqdm-' + args.f	
+	with open(args.f, 'w') as f:
 		f.write('\n'.join(lines))
 
 if __name__ == '__main__':
+
+	args = _parse_args()
+	args = handleArguments(args)
+
 	fileLines = readLinesFromFile()
 
 	modifiedFile = addTQDM(fileLines)
-	writeLinesToFile(modifiedFile)
-	a = os.system("python test.py")
+	writeLinesToFile(modifiedFile, args)
+	if args.r:
+		print(args.f)
+		a = os.system("python " + args.f)
